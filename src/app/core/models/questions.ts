e50e8dfa528d4015276ca0f0a7df8ea3145f846b
amanyzohair <amanyzohair3@gmail.com>
2025-03-16 03:38:47 +0200

    Initating Iqra network assessment project

---
+++ src/app/core/models/questions.ts
@@ -0,0 +1,28 @@
+export interface Question {
+  id: string;
+  label: string;
+  description?: string;
+  type?: string;
+  mandatory?: boolean;
+  enabled?: boolean;
+  allowed_values?: boolean;
+  allow_upload?: boolean;
+  multiple_answers?: boolean;
+  sub_questions: Question[];
+  options?: Option[];
+  multiplechoice?: boolean;
+  dependant_question?: string;
+  predecessor: string;
+  initial_state?: string;
+  min_value?: string;
+  max_value?: string;
+  currency?: any;
+  company?: any;
+}
+
+export interface Option {
+  id: string;
+  label: string;
+  question?: string;
+  dependant_questions: string[];
+}
